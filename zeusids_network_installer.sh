f61d3c6c953d533a04e393015972c0063e450b2f
Samuel Blouir <scblouir+github@gmail.com>
2021-01-18 17:06:24 -0500

    +User prompt: Remove current install?

--- zeusids_network_installer.sh
+++ zeusids_network_installer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 
@@ -8,6 +8,8 @@
 ### and enter this command:
 ###
 ###		sudo chmod +x ./zeusids_network_installer.sh && ./zeusids_network_installer.sh
+###
+### *** WARNING!!! This will erase the $HOME/$REPO_NAME directory ($REPO_NAME is set to zeusids) ***
 
 
 
@@ -18,13 +20,26 @@
 REPO_NAME="zeusids"
 REPO_ACCOUNT="scblouir"
 
+ask_if_user_wants_to_rm_install_dir(){
+	
+	read -p "The directory already exists. Perform rm -r to continue installation? y/N: " USER_ANSWER
+	case ${USER_ANSWER:0:1} in
+	    y|Y )
+		sudo rm -r /home/$USER/$REPO_NAME && mkdir /home/$USER/$REPO_NAME || exit 1
+	    ;;
+	    * )
+		exit 1
+	    ;;
+	esac
+
+}
 
 
 ##
 ##  0)  Clones the zeusids repo locally
 ##
 sudo apt install -y git || exit 1
-mkdir -p /home/$USER/$REPO
+mkdir /home/$USER/$REPO_NAME || ask_if_user_wants_to_rm_install_dir
 cd /home/$USER/ && sudo git clone https://github.com/$REPO_ACCOUNT/$REPO_NAME || exit 1
 
 
@@ -37,5 +52,4 @@ sudo chmod -R 757 $REPO_NAME && $REPO_NAME/scripts/__internal/__installation/__i
 || printf "\n\nFatal Error: Failed to run __install.sh.\n" || exit 1
 
 exit 0
-
 
