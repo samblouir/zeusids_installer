6a2e2ff611ca958fb0c1ca4bd29ae88cfa07dbc4
scblouir <scblouir+github@gmail.com>
2021-01-17 21:44:32 -0500

    Update zeusids_network_installer.sh

--- zeusids_network_installer.sh
+++ zeusids_network_installer.sh
@@ -33,6 +33,6 @@ cd /home/$USER/ && sudo git clone https://github.com/$REPO_ACCOUNT/$REPO_NAME ||
 ##
 ##  1)  Runs the installer
 ##
-sudo chmod -R 757 $REPO_NAME && $REPO_NAME/scripts/__internal/__installation/__install.sh 0 || printf "\n\nFatal Error: Failed to run __install.sh."
+sudo chmod -R 757 $REPO_NAME && $REPO_NAME/scripts/__internal/__installation/__install.sh 0 || printf "\n\nFatal Error: Failed to run __install.sh.\n"
 
 
