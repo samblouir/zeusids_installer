be105f5d79994f033f70b5b48e13673ec3eccb62
Samuel Blouir <scblouir+github@gmail.com>
2021-01-29 15:41:08 -0500

    Tidying-up code

--- zeusids_network_installer.sh
+++ zeusids_network_installer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 
-
-
 ### This script installs zeusids with Nvidia drivers, CUDA tools, and other required packages locally.
 ### To run this, open the terminal (CTRL+ALT+T), cd to the directory this file is located in,
 ### and enter this command:
 ###
 ###		sudo chmod +x ./zeusids_network_installer.sh && ./zeusids_network_installer.sh
 ###
-### *** WARNING!!! This will erase the $HOME/$REPO_NAME directory ($REPO_NAME is set to zeusids) ***
-
+### *** WARNING!!! This installer will prompt the user to confirm erasure of the $HOME/$REPO_NAME directory ($REPO_NAME is set to zeusids) ***
+###
+###
+### Todo: Add a force install option
 
 
 
@@ -20,13 +20,10 @@
 REPO_NAME="zeusids"
 REPO_ACCOUNT="samblouir"
 
+## If the install directory already exists, confirms with the user if they want to remove it.
 ask_if_user_wants_to_rm_install_dir(){
-
 	
-	
-	#printf "\nYou said: \"$USER_ANSWER\"\n"
-	
-	read -p "The directory already exists. Perform rm -r to continue installation? y/N: " USER_ANSWER
+	read -p "The directory (~/$REPO_NAME) already exists. Perform rm -r to continue installation? y/N: " USER_ANSWER
 	case ${USER_ANSWER:0:1} in
 	    y|Y )
 		sudo rm -r /home/$USER/$REPO_NAME && mkdir /home/$USER/$REPO_NAME || exit 1
@@ -35,7 +32,6 @@ ask_if_user_wants_to_rm_install_dir(){
 		exit 1
 	    ;;
 	esac
-
 }
 
 
@@ -49,12 +45,11 @@ cd /home/$USER/ && sudo git clone https://github.com/$REPO_ACCOUNT/$REPO_NAME ||
 
 
 
-
 ##
 ##  1)  Runs the installer
 ##
 sudo chmod -R 757 $REPO_NAME && $REPO_NAME/scripts/__internal/__installation/__install.sh 0 \
-|| printf "\n\nFatal Error: Failed to run __install.sh.\n" || exit 1
+|| printf "\n\nFatal Error: Failure during __install.sh.\n" || exit 1
 
 exit 0
 
